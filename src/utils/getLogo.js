3a1b862a99de9e22bbf653463d413f9e186b5c08
unknown <reza.avoor@gmail.com>
2019-12-14 11:27:01 +0100

    add Fotmob api to getLogo api

--- src/utils/getLogo.js
+++ src/utils/getLogo.js
@@ -1,41 +1,47 @@
 import axios from "axios";
 import cheerio from "cheerio";
-import getTeamId from "./getTeamId";
 
 const getLogoTransferMarket = async name => {
-  name = name.replace(" ", "%");
-  const url = `https://cors-anywhere.herokuapp.com/https://www.transfermarkt.com/schnellsuche/ergebnis/schnellsuche?query=${name}`;
-  const { data } = await axios.get(url);
-  const $ = cheerio.load(data);
-  const tables = Object.values($(".table-header"));
-  let imgSrc;
-  const fetchData = table => {
-    imgSrc =
-      table.parent.children[3].children[1].children[3].children[2].next
-        .children[0].next.children[0].next.children[0].attribs.src;
-  };
-  tables.forEach((table, index) => {
-    try {
-      table.children[0].data.includes("Clubs") && fetchData(tables[index]);
-    } catch {}
-  });
-  return imgSrc && imgSrc;
-};
-const getLogoTheSportDb = async name => {
-  const theName = name.replace(" ", "%");
-  const url = `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${theName}`;
-  const { data } = await axios.get(url);
-  if (data.teams) {
-    return data.teams[0].strTeamBadge;
-  } else return getLogoTransferMarket(name);
+  try {
+    name = name.replace(" ", "%");
+    const url = `https://cors-anywhere.herokuapp.com/https://www.transfermarkt.com/schnellsuche/ergebnis/schnellsuche?query=${name}`;
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+    const tables = Object.values($(".table-header"));
+    let imgSrc;
+    const fetchData = table => {
+      imgSrc =
+        table.parent.children[3].children[1].children[3].children[2].next
+          .children[0].next.children[0].next.children[0].attribs.src;
+    };
+    tables.forEach((table, index) => {
+      try {
+        table.children[0].data.includes("Clubs") && fetchData(tables[index]);
+      } catch {}
+    });
+    return imgSrc && imgSrc;
+  } catch {
+    return "https://www.konfest.com/wp-content/uploads/2019/05/Konfest-PNG-JPG-Image-Pic-Photo-Free-Download-Royalty-Unlimited-clip-art-sticker-Question-Mark-Symbol-Sign-Black-White-11.png";
+  }
 };
 const getLogoFotmob = async name => {
   if (name) {
-    const teamId = getTeamId(name.toLowerCase());
-    if (teamId) {
+    name = name
+      .replace("FC", "")
+      .replace("CF", "")
+      .replace("FSV", "")
+      .replace("SD", "")
+      .replace("WS", "");
+    const { data } = await axios.get(
+      `https://apigw.fotmob.com/searchapi/suggest?term=${name}`
+    );
+    if (data.teamSuggest) {
+      const [teamName, teamId] = data.teamSuggest[0].options[0].text.split("|");
       const logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${teamId}.png`;
       return logo;
-    } else return await getLogoTheSportDb(name);
+    } else {
+      return await getLogoTransferMarket(name);
+    }
   }
 };
 export default async function getLogo(name) {
